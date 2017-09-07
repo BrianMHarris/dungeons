67d8f25e5a9611cb7b1104ea537461a9b022c06c
Brian Harris <brianharrisdesign@gmail.com>
2017-09-07 14:14:05 -0700

    new color scheme

---
+++ project/static/js/home/index.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//     // Add scrollspy to <body>
+//   $('body').scrollspy({target: ".navbar", offset: 50});
+
+//   // Add smooth scrolling on all links inside the navbar
+//   $("#btn-nav-about").on('click', function(event) {
+//     debugger;
+//     var $anchor = $(this);
+//     e.preventDefault(); //this is the important line.
+//     $('html, body').stop().animate({
+//         scrollTop: $($anchor.attr('href')).offset().top
+//     }, 1500);
+
+//   });
+// });
+
